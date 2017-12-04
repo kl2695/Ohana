3024689c4388324129f0627fc99ef74a39ec65c7
kevin lee <kl2695@nyu.edu>
2017-12-04 13:04:25 -0800

    get rid of index unique constraints

--- db/migrate/20171128040617_create_likes.rb
+++ db/migrate/20171128040617_create_likes.rb
@@ -4,6 +4,6 @@ class CreateLikes < ActiveRecord::Migration[5.1]
       t.integer :moment_id, null: false
       t.integer :user_id, null: false 
     end
-    add_index(:likes, [:moment_id, :user_id], unique: true) 
+    # add_index(:likes, [:moment_id, :user_id], unique: true) 
   end
 end
