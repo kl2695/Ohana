e71a517678b515a7e882e522e825e93a4676c61e
kevin lee <kl2695@nyu.edu>
2017-12-04 15:02:02 -0800

    add action cable configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://kevinsfullstack.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://kevinsfullstack.herokuapp.com', 'http://kevinsfullstack.herokuapp.com']
 end
